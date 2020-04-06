df0ce804a4a0abc5b9ae92bbc4b7f536ff71efa9
Ben <benmason1992@hotmail.co.uk>
2020-04-06 18:15:09 +0100

    removed browser router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  HashRouter as Router, //I was thinking about adding the browser router into the app however i decided to leave as a one page app.
-  // Route,
-  // Switch,
-} from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 import Add from "../AddPlayer";
@@ -14,7 +9,7 @@ import Settings from "../Settings";
 import '../../App.css';
 
 const App = ({ numberSelected }) => (
-  <Router>
+  <>
     <Header >Play Ping Pong</Header>
     <>
       {!numberSelected ?
@@ -29,7 +24,7 @@ const App = ({ numberSelected }) => (
       }
     </>
     <Footer />
-  </Router>
+  </>
 );
 
 export default App;
